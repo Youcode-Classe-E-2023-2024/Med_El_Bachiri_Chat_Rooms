58d2b2277ce12bdf9c3989c47836ee1b9718bc48
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-12-26 00:23:10 +0100

    Update home_controller.php

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -8,7 +8,12 @@ if (empty($_SESSION['user_id'])) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     header('Content-Type: application/json');
-    $rooms = Room::getUserRooms($_SESSION['user_id']);
+    try {
+        $rooms = Room::getUserRooms($_SESSION['user_id']);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
     $data = json_decode(file_get_contents('php://input'), true);
 
     if (isset($data['room_name']) && !empty($data['room_name']))
@@ -16,14 +21,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         $room = Room::creat($_SESSION['user_id'], $data['room_name']);
         if ($room)
         {
-            array_push($rooms,['sucsess' => 'hey the room added succ']);
+            $rooms[] = ['message' => 'the room added successfully'];
             echo json_encode($rooms);
             exit();
         }
     }
     else
     {
-        array_push($rooms,['hamid' => 'the input is empty']);
+        $rooms[] = ['message' => 'the input is empty'];
         echo json_encode($rooms);
         exit();
     }
